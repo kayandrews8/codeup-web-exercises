9f4caab786dbcd7728f652e815452a61010e8250
Kaylah Andrews <kandr716@gmail.com>
2022-02-07 12:18:06 -0600

    completed morning exercise for 2/7

---
+++ js/js-warmups.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function whatYouSaid(input){
+    return "I have no idea what you just said.";
+}
+// console.log(whatYouSaid("yes"));
+
+function timesTwo(number){
+    return number * 2;
+}
+
+console.log(timesTwo(50));
