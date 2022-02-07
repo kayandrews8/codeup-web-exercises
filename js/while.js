e5718b8285fa012ae03c9b7545b82ece76829b69
Kaylah Andrews <kandr716@gmail.com>
2022-02-07 15:39:15 -0600

    completed do-while exercises

--- js/while.js
+++ js/while.js
@@ -1,6 +1,22 @@
-var counter = 1;
-var number = 1;
-while (number <= 65536){
-    console.log(number);
-    number = number *2;
-}
+
+// var number = 2;
+// while (number <= 65536){
+//     console.log(number);
+//     number = number *2;
+// }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var conesSold
+
+do{
+    conesSold = Math.floor(Math.random() * 5) +1;
+    console.log("that's " + conesSold + " sold...");
+    if(conesSold > allCones){
+        console.log("I can not sell you " + conesSold + " I only have " + allCones + " left");
+    } else {
+    allCones = allCones - conesSold;
+    console.log("I need to sell " + allCones + " more cones!");
+    }
+} while(allCones > 0);
+console.log("Yay! I sold them all!");
+
