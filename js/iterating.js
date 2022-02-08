060b09b07c4deaf24ea8f578f973d80a16068a0f
Kaylah Andrews <kandr716@gmail.com>
2022-02-08 15:48:34 -0600

    complete arrays exercise

---
+++ js/iterating.js
@@ -0,0 +1,75 @@
+// (function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+    var peopleNames = ['Moira', 'Tony', 'Kaylah', 'Jelani'];
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+    // console.log(names.length);
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+    // console.log(peopleNames[0]);
+    // console.log(peopleNames[1]);
+    // console.log(peopleNames[2]);
+    // console.log(peopleNames[3]);
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+    // for(var i = 0; i < peopleNames.length; i ++){
+    //     if(i !== peopleNames - 1){
+    //         console.log(peopleNames[i]);
+    //     }
+    // }
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+//     peopleNames.forEach(function(name){
+//     console.log(name);
+// });
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+    // function firstItem(array){
+    //     return array[0];
+    // }
+    // console.log(firstItem(['1', '2', '3', '4', '5']));
+    //
+    // function secondItem(array){
+    //     return array[1];
+    // }
+    // console.log(secondItem(['1', '2', '3', '4', '5']));
+    //
+    // function lastItem(array){
+    //     return array[array.length -1];
+    // }
+    // console.log(lastItem(['1', '2', '3', '4', '5']));
+
+// })();
