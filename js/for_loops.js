571b00df0dff03426d0f824410d892c8a61f60b2
Kaylah Andrews <kandr716@gmail.com>
2022-02-08 11:05:34 -0600

    complete loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,33 @@
+
+// for (var count = 100; count >= 0; count = count -1){
+//     console.log(count);
+// }
+
+
+// function showMultiplicationTable(num){
+//     for (var i = 1; i <= 10; i++){
+//         console.log(num + " x " + i + " = " + (num * i));
+//     }
+// }
+// console.log(showMultiplicationTable(4));
+
+
+
+// for(var i = 0; i < 10; i++ ){
+//     var randomNumber = Math.floor(Math.random() * 181) + 20;
+//     if(randomNumber % 2 === 0){
+//         console.log(randomNumber + " is even");
+//     } else {
+//         console.log(randomNumber + " is odd");
+//     }
+// }
+
+for (var number = 1; number < 10; number++){
+    var count = 0;
+    var emptyStr = "";
+    while (count < number){
+        emptyStr = emptyStr + number.toString();
+        count = count + 1;
+    }
+    console.log(emptyStr);
+}
